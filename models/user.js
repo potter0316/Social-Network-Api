bfc55a1211a57ce7480242ea705a8b87e93b8c3e
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-05-15 20:48:07 -0500

    model User.js finished

--- models/user.js
+++ models/user.js
@@ -1,1 +1,40 @@
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ]
+},
+    {
+        toJson: {
+            virtuals: true,
+            getters: true
+        }
+    });
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("User", userSchema);
+module.exports = User;
